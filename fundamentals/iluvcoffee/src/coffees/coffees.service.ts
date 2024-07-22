97bd0781511e32e5f848709934c443acaf4de7f6
Wilmer Arteaga <killerwilmer@gmail.com>
2024-07-21 21:43:54 -0500

    Handling malicios request data

--- fundamentals/iluvcoffee/src/coffees/coffees.service.ts
+++ fundamentals/iluvcoffee/src/coffees/coffees.service.ts
@@ -28,6 +28,8 @@ export class CoffeesService {
 
   create(createCoffeeDto: any) {
     this.coffees.push(createCoffeeDto);
+
+    return createCoffeeDto;
   }
 
   update(id: string, updateCoffeeDto: any) {
