e2f0201419f6f4b6cc52d01d1f1b4ffda06386d7
Wilmer Arteaga <killerwilmer@gmail.com>
2024-08-16 10:29:31 -0500

    Retrieve Entities with their Relations

--- fundamentals/iluvcoffee/src/coffees/coffees.service.ts
+++ fundamentals/iluvcoffee/src/coffees/coffees.service.ts
@@ -13,11 +13,16 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: ['flavors'],
+    });
   }
 
   async findOne(id: string) {
-    const coffee = await this.coffeeRepository.findOne({ where: { id: +id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id: +id },
+      relations: ['flavors'],
+    });
 
     if (!coffee) {
       throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
