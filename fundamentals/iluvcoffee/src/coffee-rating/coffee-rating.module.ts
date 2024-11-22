5ec111b1b1152a5d7d9d87ea0c717c88930b022d
Wilmer Arteaga <killerwilmer@gmail.com>
2024-11-21 22:19:32 -0500

    Create a Dynamic Module

--- fundamentals/iluvcoffee/src/coffee-rating/coffee-rating.module.ts
+++ fundamentals/iluvcoffee/src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffeesModule } from '../coffees/coffees.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    DatabaseModule.register({
+      // 👈 passing in dynamic values
+      type: 'postgres',
+      host: 'localhost',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+    }),
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
