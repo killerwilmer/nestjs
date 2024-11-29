17de162e93bf50c56ce4e69dd10a58c2b453a1bc
Wilmer Arteaga <killerwilmer@gmail.com>
2024-11-29 17:30:08 -0500

    Catch Exceptions with Filters

---
+++ fundamentals/iluvcoffee/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,32 @@
+import {
+  Catch,
+  HttpException,
+  ExceptionFilter,
+  ArgumentsHost,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter<T extends HttpException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+    const error =
+      typeof exceptionResponse === 'string'
+        ? {
+            message: exceptionResponse,
+            statusCode: status,
+          }
+        : exceptionResponse;
+
+    response.status(status).json({
+      ...error,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
