d2e9f86a1587277f331addf53c71f98c4d28caf2
Wilmer Arteaga <killerwilmer@gmail.com>
2024-12-07 18:03:56 -0500

    Getting Started with Test Suites

--- fundamentals/iluvcoffee/src/coffees/coffees.service.spec.ts
+++ fundamentals/iluvcoffee/src/coffees/coffees.service.spec.ts
@@ -1,15 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
+import { DataSource } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Flavor } from './entities/flavor.entity';
+import { Coffee } from './entities/coffee.entity';
+import { ConfigService } from '@nestjs/config';
+import coffeesConfig from './config/coffees.config';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        {
+          provide: DataSource,
+          useValue: {},
+        },
+        { provide: getRepositoryToken(Flavor), useValue: {} },
+        {
+          provide: getRepositoryToken(Coffee),
+          useValue: {},
+        },
+        {
+          provide: ConfigService,
+          useValue: {},
+        },
+        {
+          provide: coffeesConfig.KEY,
+          useValue: { foo: 'mockedFooValue' },
+        },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
+    // service = await module.resolve(CoffeesService); // retrieve Request Scoped or Transient Scoped providers
   });
 
   it('should be defined', () => {
