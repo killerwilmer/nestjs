89084d47ca5268f8e52fb2adb6467c68190b5cec
Wilmer Arteaga <killerwilmer@gmail.com>
2024-12-17 13:28:24 -0500

    run command nest g resource workflows and update port on main.ts workflows

--- microservices/virtual-facility/apps/workflows-service/src/main.ts
+++ microservices/virtual-facility/apps/workflows-service/src/main.ts
@@ -3,6 +3,6 @@ import { WorkflowsServiceModule } from './workflows-service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(WorkflowsServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
