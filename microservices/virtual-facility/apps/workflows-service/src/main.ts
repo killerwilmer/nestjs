bf041066aee73a067731d08a64cf2bfcc10f4eba
Wilmer Arteaga <killerwilmer@gmail.com>
2024-12-17 21:31:12 -0500

    Configuring Docker Compose

--- microservices/virtual-facility/apps/workflows-service/src/main.ts
+++ microservices/virtual-facility/apps/workflows-service/src/main.ts
@@ -3,6 +3,6 @@ import { WorkflowsServiceModule } from './workflows-service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(WorkflowsServiceModule);
-  await app.listen(process.env.port ?? 3001);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
