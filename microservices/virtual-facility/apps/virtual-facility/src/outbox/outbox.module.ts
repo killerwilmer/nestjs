d47eee9513926f7466beaca6675cdc4daef65516
Wilmer Arteaga <wilmerarteaga@Wilmers-MacBook-Pro.local>
2025-01-13 22:55:20 -0500

    Transactional Outbox Pattern

---
+++ microservices/virtual-facility/apps/virtual-facility/src/outbox/outbox.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { OutboxService } from './outbox.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Outbox } from './entities/outbox.entity';
+import { WORKFLOWS_SERVICE } from '../constants';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { OutboxProcessor } from './outbox.processor';
+import { OutboxEntitySubscriber } from './outbox.entity-subscriber';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Outbox]),
+    ClientsModule.register([
+      {
+        name: WORKFLOWS_SERVICE,
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: 'workflows-service',
+        },
+      },
+    ]),
+  ],
+
+  providers: [OutboxService, OutboxProcessor, OutboxEntitySubscriber],
+})
+export class OutboxModule {}
