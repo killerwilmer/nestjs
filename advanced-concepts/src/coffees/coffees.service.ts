2b22fd153d18e5593590bc3827854aef6a1cc239
Wilmer Arteaga <killerwilmer@gmail.com>
2025-04-30 22:08:44 -0500

    Lazy-loading Modules nest g mo rewards --skip-import nest g s rewards curl -H 'content-type: application/json' localhost:3000/coffees -d "{}"

--- advanced-concepts/src/coffees/coffees.service.ts
+++ advanced-concepts/src/coffees/coffees.service.ts
@@ -3,6 +3,7 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 import { COFFEES_DATA_SOURCE } from './constants';
+import { LazyModuleLoader } from '@nestjs/core';
 
 // OR alternatively "export type CoffeesDataSource = Coffee[]"
 export interface CoffeesDataSource {
@@ -11,9 +12,24 @@ export interface CoffeesDataSource {
 
 @Injectable()
 export class CoffeesService {
-  constructor(@Inject(COFFEES_DATA_SOURCE) dataSource: CoffeesDataSource) {} // 👈
+  constructor(
+    @Inject(COFFEES_DATA_SOURCE) dataSource: CoffeesDataSource,
+    private readonly lazyModuleLoader: LazyModuleLoader,
+  ) {} // 👈
 
-  create(createCoffeeDto: CreateCoffeeDto) {
+  async create(createCoffeeDto: CreateCoffeeDto) {
+    console.time();
+    const rewardsModuleRef = await this.lazyModuleLoader.load(() =>
+      import('../rewards/rewards.module').then((m) => m.RewardsModule),
+    );
+
+    const { RewardsService } = await import('../rewards/rewards.service');
+    const rewardsService = rewardsModuleRef.get(RewardsService);
+
+    console.timeEnd();
+
+    rewardsService.grantTo();
+
     return 'This action adds a new coffee';
   }
 
