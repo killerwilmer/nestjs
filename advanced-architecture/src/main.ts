3e1ecc89c8c5c48a84eb36c2d7f6ad57045d2d3a
Wilmer Arteaga <killerwilmer@gmail.com>
2025-06-07 18:13:32 -0500

    Hexagonal Architecture in Practice. Part 2 npm i @nestjs/typeorm typeorm pg nest g mo core docker-compose up -d curl -X POST http://localhost:3000/alarms -H "Content-Type: application/json" -d '{"name": "Test Alarm", "severity": "high"}' curl http://localhost:3000/alarms

--- advanced-architecture/src/main.ts
+++ advanced-architecture/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule.register({
+      driver: 'orm', // 👈 or 'in-memory'
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
