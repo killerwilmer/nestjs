4c8481830959fdbc668a919480d2a32a9458977b
Wilmer Arteaga <killerwilmer@gmail.com>
2025-06-19 21:16:38 -0500

    Adding an Event Store. Part 2 curl --location --request POST 'localhost:3000/alarms' \ --header 'Content-Type: application/json' \ --data-raw '{     "name": "Lesson completed!",     "severity": "HIGH",     "triggeredAt": "2021-01-01T00:00:00.000Z",     "items": [         {             "name": "Item 1",             "type": "TYPE_1"         },         {             "name": "Item 2",             "type": "TYPE_2"         }     ] }' | json_pp curl --location --request GET 'localhost:3000/alarms' | json_pp

--- advanced-architecture/src/alarms/application/alarms.module.ts
+++ advanced-architecture/src/alarms/application/alarms.module.ts
@@ -4,7 +4,7 @@ import { AlarmsService } from './alarms.service';
 import { AlarmFactory } from '../domain/factories/alarm.factory';
 import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
 import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
-import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
+import { AlarmCreatedEventHandler } from './event-handlers/alarm-created-event-handler';
 
 @Module({
   controllers: [AlarmsController],
