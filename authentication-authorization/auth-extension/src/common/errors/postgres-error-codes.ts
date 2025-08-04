f6de3cecb686eee8a31b78973d3e9512e9fca42e
Wilmer Arteaga <killerwilmer@gmail.com>
2025-08-04 14:34:19 -0500

    nest g controller iam/authentication nest g service iam/authentication
    nest g class iam/authentication/dto/sign-in.dto --no-spec --flat
    nest g class iam/authentication/dto/sign-up.dto --no-spec --flat
    
    npm i class-validator class-transformer

---
+++ authentication-authorization/auth-extension/src/common/errors/postgres-error-codes.ts
@@ -0,0 +1,5 @@
+export const PostgresErrorCodes = {
+  UNIQUE_VIOLATION: '23505',
+  NOT_NULL_VIOLATION: '23502',
+  FOREIGN_KEY_VIOLATION: '23503',
+} as const;
---
+++ authentication-authorization/auth-extension/src/iam/authentication/authentication.service.ts
@@ -0,0 +1,61 @@
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { PostgresError } from '../../common/errors/postgres-error.interface';
+import { PostgresErrorCodes } from '../../common/errors/postgres-error-codes';
+import { SignInDto } from './dto/sign-in.dto';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.password);
+
+      await this.userRepository.save(user);
+    } catch (error) {
+      const err = error as PostgresError;
+
+      const pgUniqueViolationError = PostgresErrorCodes.UNIQUE_VIOLATION;
+      if (err.code === pgUniqueViolationError) {
+        throw new ConflictException();
+      }
+      throw err;
+    }
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const user = await this.userRepository.findOneBy({
+      email: signInDto.email,
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User does not exist');
+    }
+
+    const isEqual = await this.hashingService.compare(
+      signInDto.password,
+      user.password,
+    );
+
+    if (!isEqual) {
+      throw new UnauthorizedException('Password does not match');
+    }
+
+    // TODO: We'll fill this gap in the next step
+    return true;
+  }
+}
