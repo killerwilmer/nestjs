4afec7480fada31f7722e766f3e1e10d529db29f
Wilmer Arteaga <killerwilmer@gmail.com>
2025-08-13 15:12:09 -0500

    Integrate API Keys feature nest g class users/api-keys/entities/api-key.entity --no-spec --flat nest g service iam/authentication/api-keys --flat nest g guard iam/authentication/guards/api-key --flat npm run start -- --entryFile repl uuid = 'random_unique_id' payload = await get(ApiKeysService).createAndHash(uuid) await get("ApiKeyRepository").save({ uuid, key: payload.hashedKey, user: { id: 1 }})

--- authentication-authorization/auth-extension/src/iam/authentication/guards/authentication.guard.ts
+++ authentication-authorization/auth-extension/src/iam/authentication/guards/authentication.guard.ts
@@ -8,6 +8,7 @@ import { Reflector } from '@nestjs/core';
 import { AUTH_TYPE_KEY } from '../decorators/auth.decorator';
 import { AuthType } from '../enums/auth-type.enum';
 import { AccessTokenGuard } from './access-token.guard';
+import { ApiKeyGuard } from './api-key.guard';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
@@ -20,9 +21,11 @@ export class AuthenticationGuard implements CanActivate {
   constructor(
     private readonly reflector: Reflector,
     private readonly accessTokenGuard: AccessTokenGuard,
+    private readonly apiKeyGuard: ApiKeyGuard,
   ) {
     this.authTypeGuardMap = {
       [AuthType.Bearer]: this.accessTokenGuard,
+      [AuthType.ApiKey]: this.apiKeyGuard,
       [AuthType.None]: { canActivate: () => true },
     };
   }
--- authentication-authorization/auth-extension/src/users/entities/user.entity.ts
+++ authentication-authorization/auth-extension/src/users/entities/user.entity.ts
@@ -1,9 +1,16 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Role } from '../enums/role.enum';
 import {
   Permission,
   PermissionType,
 } from '../../iam/authorization/permission.type';
+import { ApiKey } from '../api-keys/entities/api-key.entity';
 
 @Entity()
 export class User {
@@ -18,6 +25,9 @@ export class User {
 
   @Column({ enum: Role, default: Role.Regular })
   role: Role;
+
+  @OneToMany((type) => ApiKey, (apiKey) => apiKey.user) // <- inverse side
+  apiKeys: ApiKey[];
 
   // NOTE: Having the "permissions" column in combination with the "role"
   // likely does not make sense. We use both in this course just to showcase
