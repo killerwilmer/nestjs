47187a240a2a1544ddf0d7013ee1ad1f12a14d0a
Wilmer Arteaga <killerwilmer@gmail.com>
2025-09-03 15:13:38 -0500

    Batching and Caching with Data Loader N+1 problem npm install --save dataloader --flat nest g provider coffees/data-loader/flavors-by-coffee.loader --flat

--- graphql-schema-first/graphql-coffee/src/coffees/coffee-flavors.resolver.ts
+++ graphql-schema-first/graphql-coffee/src/coffees/coffee-flavors.resolver.ts
@@ -1,26 +1,18 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Flavor } from './entities/flavor.entity';
-import { Repository } from 'typeorm';
 import { Coffee } from './entities/coffee.entity';
+import { FlavorsByCoffeeLoader } from './data-loader/flavors-by-coffee.loader';
 
 @Resolver('Coffee')
 export class CoffeeFlavorsResolver {
   constructor(
     // ⚙️ Inject the Flavor Repository
-    @InjectRepository(Flavor)
-    private readonly flavorsRepository: Repository<Flavor>,
+    private readonly flavorsByCoffeeLoader: FlavorsByCoffeeLoader, // 👈 utilize our new loader
   ) {}
 
   @ResolveField('flavors')
   async getFlavorsOfCoffee(@Parent() coffee: Coffee) {
     // Using the injected repository,
     // let’s retrieve ALL flavors that belong to a “parent coffee”.
-    return this.flavorsRepository
-      .createQueryBuilder('flavor') // 'flavor' is an alias
-      .innerJoin('flavor.coffees', 'coffees', 'coffees.id = :coffeeId', {
-        coffeeId: coffee.id,
-      })
-      .getMany();
+    return this.flavorsByCoffeeLoader.load(coffee.id); // 👈
   }
 }
