bcb0698fb73d6ff7a8d219b366253651bfa30011
Wilmer Arteaga <killerwilmer@gmail.com>
2025-09-04 15:08:43 -0500

    Config run scripts in turbo start:dev isntall microservices in the root npm install @nestjs/microservices

--- turbo/microservices/apps/users-service/src/main.ts
+++ turbo/microservices/apps/users-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: 4003,
+    },
+  });
+  await app.listen();
+  console.log('Users Microservice is listening on port 4003');
 }
 bootstrap();
