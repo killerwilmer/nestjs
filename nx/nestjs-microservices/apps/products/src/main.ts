495fe00ca3ef8904540548aafa8784e86f48b7a4
Wilmer Arteaga <killerwilmer@gmail.com>
2025-09-08 22:38:26 -0500

    Config .proto nx.json   "targetDefaults": {     "test": {       "dependsOn": ["^build"]     },     "build": {       "options": {         "assets": [           {             "input": "libs/types/src/proto",             "glob": "*.proto",             "output": "proto"           }         ]       }     }   }

--- nx/nestjs-microservices/apps/products/src/main.ts
+++ nx/nestjs-microservices/apps/products/src/main.ts
@@ -6,15 +6,23 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { PRODUCTS_PACKAGE_NAME } from '@nestjs-microservices/types';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3001;
-  await app.listen(port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: PRODUCTS_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/products.proto'),
+    },
+  });
+
+  await app.listen();
+
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on gRPC channel`
   );
 }
 
